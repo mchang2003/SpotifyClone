e3ccfd20c924aded7764b16d1b7440ca0ddccb7d
mchang2003 <91290209+mchang2003@users.noreply.github.com>
2022-08-21 18:38:00 -0400

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import "./App.css";
+import Login from "./Login"
+
+function App() {
+  return (
+    // does it matter if use ' vs " for classname?
+    <div className='app'> 
+      <Login />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/spotify.js
@@ -0,0 +1,19 @@
+
+
+export const authEndpoint = "https://accounts.spotify.com/authorize";
+
+const redirectUri = "http://localhost:3000/";
+
+const clientId = "f0925fff00a1462291267e6ea0ba0aa6";
+
+const scopes = [
+    "user-read-currently-playing",
+    "user-read-recently-played",
+    "user-read-playback-state",
+    "user-top-read",
+    "user-modify-playback-state",
+];
+
+export const loginUrl =  `${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join(
+    "%20"
+  )}&response_type=token&show_dialog=true`;
